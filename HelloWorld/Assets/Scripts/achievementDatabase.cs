a84f27771cdf2d10fdf0cb6bdb66fc110d370a5a
Victor Morgant <victor.morgant@gmail.com>
2020-01-16 14:17:04 +0100

    achievement data system

---
+++ HelloWorld/Assets/Scripts/achievementDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Malee;
+    [CreateAssetMenu()]
+public class achievementDatabase : ScriptableObject
+{
+    [Reorderable]
+    public achievementsArray achievements;
+    [System.Serializable]
+    public class achievementsArray : ReorderableArray<achievement> { }
+}
